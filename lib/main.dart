73ccc396101628b28b19c046f202202c12ffeecd
malikkashifawan7 <malik.kashif28888@gmail.com>
2026-08-22 13:53:46 +0500

    Fix navigation on tap for GridView cards

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,8 @@ class _HomeState extends State<Home>{
    GridView.count(shrinkWrap:true,physics:const NeverScrollableScrollPhysics(),crossAxisCount:2,
     crossAxisSpacing:9,mainAxisSpacing:9,childAspectRatio:1.7,
     children:modules.entries.map((e)=>Card(child:InkWell(
-     onTap:()=>Navigator.push(context,MaterialPageRoute(builder:(_)=>Page(.db db,title:e.key))),
+     onTap:()=>Navigator.push(context,MaterialPageRoute(builder:(_) Page(.db db,title:e.key)
+                                                        
      child:Column(mainAxisAlignment:MainAxisAlignment.center,children:[
       Icon(e.value,size:30),const SizedBox(height:6),Text(e.key,textAlign:TextAlign.center)
      ])))).toList())
