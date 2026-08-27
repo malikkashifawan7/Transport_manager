eaaf21d1efa3f1dbad5b6b4ab95a089a1f290129
malikkashifawan7 <malik.kashif28888@gmail.com>
2026-08-28 00:03:27 +0500

    Update updater_service.dart

--- lib/updater_service.dart
+++ lib/updater_service.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_installer/flutter_app_installer.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AutoUpdater {
   static const String apkUrl = "https://github.com/malikkashifawan7/Transport_manager/releases/latest/download/app-release.apk";
 
   static void checkAndDownloadUpdate(BuildContext context) async {
-    final FlutterAppInstaller appInstaller = FlutterAppInstaller();
-    try {
-      await appInstaller.installApk(filePath: apkUrl);
-    } catch (e) {
-      debugPrint("Update error: $e");
+    final Uri url = Uri.parse(apkUrl);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
+    } else {
+      debugPrint("Could not launch $apkUrl");
     }
   }
 }
-
