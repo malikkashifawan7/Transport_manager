efb5f57e2a395e9e3b9d590ebf9683270ddec99c
malikkashifawan7 <malik.kashif28888@gmail.com>
2026-08-28 17:38:51 +0500

    Refactor main app and remove unused HomeDashboardView
    Refactored main app structure and removed HomeDashboardView.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
-import 'screens/fuel_screen.dart';
-import 'updater_service.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'vehicles_screen.dart';
-import 'bookings_screen.dart';
-import 'drivers_screen.dart';
-import 'auto_average_screen.dart';
+import 'screens/fuel_screen.dart';
+// Baaki aapki baki screens ke imports (vehicles_screen.dart, drivers_screen.dart, bookings_screen.dart, etc.) yahan aayenge
 
 void main() {
-  runApp(const TransportApp());
+  runApp(const MyApp());
 }
 
-class TransportApp extends StatelessWidget {
-  const TransportApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +22,13 @@ class TransportApp extends StatelessWidget {
       home: const MainHomeScreen(),
     );
   }
+}
+
+class MainHomeScreen extends StatefulWidget {
+  const MainHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainHomeScreen> createState() => _MainHomeScreenState();
 }
 
 class _MainHomeScreenState extends State<MainHomeScreen> {
@@ -79,95 +81,6 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
             label: 'Bookings',
           ),
         ],
-      ),
-    );
-  }
-}
-
-class HomeDashboardView extends StatelessWidget {
-  const HomeDashboardView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Awan Brothers Tours & Travels', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
-        backgroundColor: const Color(0xFF1A237E),
-        elevation: 0,
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Management Features',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black87),
-            ),
-            const SizedBox(height: 16),
-            GridView.count(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              crossAxisCount: 2,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              childAspectRatio: 1.1,
-              children: [
-                _buildCard(context, 'Party Bookings', Icons.bookmark_add, Colors.purple.shade50, Colors.purple, () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => const BookingsScreen()));
-                }),
-                _buildCard(context, 'Vehicle Fleet', Icons.directions_bus, Colors.blue.shade50, Colors.blue, () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => const VehiclesScreen()));
-                }),
-                _buildCard(context, 'Driver & Salary', Icons.person, Colors.orange.shade50, Colors.orange, () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => const DriversScreen()));
-                }),
-                _buildCard(context, 'Udhar Khata', Icons.account_balance_wallet, Colors.green.shade50, Colors.green, () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => const VehiclesScreen()));
-                }),
-                _buildCard(context, 'Auto Average', Icons.calculate, Colors.purple.shade50, Colors.purple.shade400, () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => const AutoAverageScreen()));
-                }),
-                _buildCard(context, 'Google Map / Route', Icons.map, Colors.red.shade50, Colors.red.shade400, () async {
-                  final Uri url = Uri.parse('https://www.google.com/maps');
-                  await launchUrl(url, mode: LaunchMode.externalApplication);
-                }),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildCard(BuildContext context, String title, IconData icon, Color bgColor, Color iconColor, VoidCallback onTap) {
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(16),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(16),
-          boxShadow: [
-            BoxShadow(color: Colors.black.withAlpha(10), blurRadius: 10, offset: const Offset(0, 4)),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 28,
-              backgroundColor: bgColor,
-              child: Icon(icon, color: iconColor, size: 28),
-            ),
-            const SizedBox(height: 12),
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.black87),
-            ),
-          ],
-        ),
       ),
     );
   }
